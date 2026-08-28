447bab879d6aeb77fd65f017bfaf9848ae2ff0f7
yiftekhar-creator <yameen.iftekhar@gmail.com>
2026-08-29 00:48:38 +0600

    feat(issues): add POST /api/issues route (createIssue)

--- backend/routes/issuesRoutes.js
+++ backend/routes/issuesRoutes.js
@@ -7,6 +7,7 @@ const auth = require('../middleware/auth');
 const requireAuth = auth && (auth.requireAuth || auth) ;
 
 const issuesController = require('../controllers/issuesController');
+const { createIssue } = require('../controllers/issuesCreateController');
 
 // Public
 router.get('/issues', asyncWrapper(issuesController.listIssues));
@@ -15,5 +16,6 @@ router.get('/issues/stats', asyncWrapper(issuesController.stats));
 
 // Authenticated
 router.get('/issues/my-reports', requireAuth, asyncWrapper(issuesController.myReports));
+router.post('/issues', requireAuth, asyncWrapper(createIssue));
 
 module.exports = router;
